87764075732dde3d834628dfc0dd5291a505b638
guillaumeprevot <guillaumeprevot@users.noreply.github.com>
2018-11-25 13:54:09 +0100

    réorganisation dans Filters du code lié à l'authentification HTTP basique (désactivée actuellement)

--- src/fr/techgp/nimbus/controllers/Filters.java
+++ src/fr/techgp/nimbus/controllers/Filters.java
@@ -14,14 +14,14 @@ public class Filters extends Controller {
 
 	/** Ce filtre s'assure que l'utilisateur est authentifié */
 	public static final Filter filterAuthenticatedOrRedirect = (request, response) -> {
-		String login = request.session().attribute("userLogin");
+		String login = getLogin(request, response, false);
 		if (login == null)
 			redirect(request, response);
 	};
 
 	/** Ce filtre s'assure que l'utilisateur est authentifié en tant qu'administrateur */
 	public static final Filter filterAdministratorOrRedirect = (request, response) -> {
-		String login = request.session().attribute("userLogin");
+		String login = getLogin(request, response, false);
 		if (login == null)
 			redirect(request, response);
 		User user = User.findByLogin(login);
@@ -42,14 +42,14 @@ public class Filters extends Controller {
 
 	/** Ce filtre s'assure que l'utilisateur est authentifié */
 	public static final Filter filterAuthenticated = (request, response) -> {
-		String login = request.session().attribute("userLogin");
+		String login = getLogin(request, response, false);
 		if (login == null)
 			SparkUtils.haltUnauthorized();
 	};
 
 	/** Ce filtre s'assure que l'utilisateur est authentifié en tant qu'administrateur */
 	public static final Filter filterAdministrator = (request, response) -> {
-		String login = request.session().attribute("userLogin");
+		String login = getLogin(request, response, false);
 		if (login == null)
 			SparkUtils.haltUnauthorized();
 		User user = User.findByLogin(login);
@@ -58,34 +58,35 @@ public class Filters extends Controller {
 	};
 
 	/**
-	 * Ce filtre s'assure que l'utilisateur est authentifié ou qu'il fournit une authentification HTTP basique valide.
+	 * Cette filtre vérifie si l'utilisateur est déjà authentifié.
+	 * Sinon, elle permet à l'utilisateur de s'authentifier via une authentification HTTP basique.
+	 * Enfin, si les 2 méthodes n'aboutissent pas, elle retourne une erreur HTTP 401 Unauthorized.
 	 *
 	 *  @see https://en.wikipedia.org/wiki/Basic_access_authentication
 	 *  @see Controller#authenticate(String, String)
 	 */
-	public static final Filter filterAuthenticatedBasic = (request, response) -> {
+	public static final String getLogin(Request request, Response response, boolean allowBasicAuthentication) {
 		// L'utilisateur est-il déjà connecté ?
 		String login = request.session().attribute("userLogin");
-		if (login != null)
-			return;
-		// L'utilisateur n'est pas connecté mais envoie-t-il ses identifiants ?
-		String authorization = request.headers("Authorization");
-		if (authorization != null && authorization.startsWith("Basic")) {
-			// On décode
-			String base64Credentials = authorization.substring("Basic".length()).trim();
-			String textCredentials = new String(Base64.getDecoder().decode(base64Credentials));
-			String[] loginAndPassword = textCredentials.split(":", 2);
-			// On vérifie login/password
-			String error = authenticate(loginAndPassword[0], loginAndPassword[1]);
-			// Authentification réussie
-			if (error == null)
-				login = loginAndPassword[0];
+		if (login == null && allowBasicAuthentication) {
+			// L'utilisateur n'est pas connecté mais envoie-t-il ses identifiants ?
+			String authorization = request.headers("Authorization");
+			if (authorization != null && authorization.startsWith("Basic")) {
+				// On décode
+				String base64Credentials = authorization.substring("Basic".length()).trim();
+				String textCredentials = new String(Base64.getDecoder().decode(base64Credentials));
+				String[] loginAndPassword = textCredentials.split(":", 2);
+				// On vérifie login/password
+				String error = authenticate(loginAndPassword[0], loginAndPassword[1]);
+				// Authentification réussie
+				if (error == null)
+					login = loginAndPassword[0];
+			}
+			// Demande d'authentification
+			if (login == null)
+				response.header("WWW-Authenticate", "Basic realm=\"Authentication required\"");
 		}
-		// Demande d'authentification
-		if (login == null) {
-			response.header("WWW-Authenticate", "Basic realm=\"Authentication required\"");
-			SparkUtils.haltUnauthorized();
-		}
-	};
+		return login;
+	}
 
 }
