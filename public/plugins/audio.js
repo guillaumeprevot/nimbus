854b77bd8697a3d885bf160e5d75cb889bd29703
guillaumeprevot <guillaumeprevot@users.noreply.github.com>
2019-01-15 21:24:58 +0100

    ajout de 2 actions sur les dossiers contenant de l'audio afin d'écouter ou d'ajouter son contenu

--- public/plugins/audio.js
+++ public/plugins/audio.js
@@ -1,9 +1,20 @@
 (function() {
+	var lastAudioFolderCheckItems = [];
 
-	function execute(play, item) {
+	function isAudioFolder(item, extension) {
+		return $.get('/items/list?recursive=false&folders=false&deleted=false&parentId=' + item.id).then(function(items) {
+			lastAudioFolderCheckItems = items.filter(function(item) {
+				var extension = item.name.substring(item.name.lastIndexOf('.') + 1);
+				return NIMBUS.utils.isBrowserSupportedAudio(item, extension);
+			});
+			return lastAudioFolderCheckItems.length > 0;
+		});
+	}
+
+	function execute(play, items) {
 		localStorage.setItem('updatePlaylist', JSON.stringify({
 			play: play,
-			items:  item ? [item] : $('#items tr.audio').get().map((tr) => $(tr).data('item'))
+			items:  items
 		}));
 		setTimeout(function() {
 			var stillHere = !!localStorage.getItem('updatePlaylist');
@@ -51,25 +62,37 @@
 			icon: 'music_note',
 			caption: 'AudioActionPlay',
 			accept: NIMBUS.utils.isBrowserSupportedAudio,
-			execute: function(item) { execute(true, item); }
+			execute: function(item) { execute(true, [item]); }
 		}, {
 			name: 'audio-play-folder',
 			icon: 'music_note',
 			caption: 'AudioActionPlayFolder',
 			accept: NIMBUS.utils.isBrowserSupportedAudio,
-			execute: function(item) { execute(true, null); }
+			execute: function(item) { execute(true, $('#items tr.audio').get().map((tr) => $(tr).data('item'))); }
+		}, {
+			name: 'audio-play-on-folder',
+			icon: 'music_note',
+			caption: 'AudioActionPlayFolder',
+			accept: isAudioFolder,
+			execute: function(item) { execute(true, lastAudioFolderCheckItems); }
 		}, {
 			name: 'audio-add',
 			icon: 'queue_music',
 			caption: 'AudioActionAdd',
 			accept: NIMBUS.utils.isBrowserSupportedAudio,
-			execute: function(item) { execute(false, item); }
+			execute: function(item) { execute(false, [item]); }
 		}, {
 			name: 'audio-add-folder',
 			icon: 'queue_music',
 			caption: 'AudioActionAddFolder',
 			accept: NIMBUS.utils.isBrowserSupportedAudio,
-			execute: function(item) { execute(false, null); }
+			execute: function(item) { execute(false, $('#items tr.audio').get().map((tr) => $(tr).data('item'))); }
+		}, {
+			name: 'audio-add-on-folder',
+			icon: 'queue_music',
+			caption: 'AudioActionAddFolder',
+			accept: isAudioFolder,
+			execute: function(item) { execute(false, lastAudioFolderCheckItems); }
 		}],
 		langs: {
 			fr: {
