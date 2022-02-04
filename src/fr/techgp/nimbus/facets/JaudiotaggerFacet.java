628c36ef89a6d5cd413a86a67cd7fccd43ee191a
guillaumeprevot <guillaumeprevot@users.noreply.github.com>
2022-02-04 20:49:36 +0100

    Amélioration de JaudiotaggerFacet (nouvelle version 3.0.1 + simplification de la gestion oga/ogg + éviter les traces inutiles sur certains mp4) - org.jaudiotagger.audio.exceptions.CannotReadVideoException dans JaudiotaggerFacet sur l'élément n°36957 (xxx.mp4) : This file appears to be an Mp4 Video file, video files are not supported - org.jaudiotagger.audio.exceptions.CannotReadException dans JaudiotaggerFacet sur l'élément n°36561 (xxx.mp4) : /path/to/file:Unable to find next atom because identifier is invalid mdat - org.jaudiotagger.audio.mp4.atom.Mp4BoxHeader seekWithinLevel \ GRAVE: Skipped 64bit data length, now at:70788625

--- src/fr/techgp/nimbus/facets/JaudiotaggerFacet.java
+++ src/fr/techgp/nimbus/facets/JaudiotaggerFacet.java
@@ -3,11 +3,14 @@ package fr.techgp.nimbus.facets;
 import java.io.File;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.jaudiotagger.audio.AudioFile;
 import org.jaudiotagger.audio.AudioFileIO;
 import org.jaudiotagger.audio.AudioHeader;
 import org.jaudiotagger.audio.SupportedFileFormat;
+import org.jaudiotagger.audio.exceptions.CannotReadVideoException;
 import org.jaudiotagger.tag.FieldKey;
 import org.jaudiotagger.tag.Tag;
 
@@ -23,13 +26,10 @@ public class JaudiotaggerFacet implements Facet {
 
 	@Override
 	public void init(Configuration configuration) {
-		// https://bitbucket.org/ijabz/jaudiotagger/src/6b4d904df18b76e932085259d676c706dcc0cb2d/src/org/jaudiotagger/audio/AudioFileIO.java?at=master&fileviewer=file-view-default
+		// https://bitbucket.org/ijabz/jaudiotagger/src/master/src/org/jaudiotagger/audio/AudioFileIO.java
 		for (SupportedFileFormat format : SupportedFileFormat.values()) {
 			extensions.add(format.getFilesuffix());
 		}
-		// SupportedFileFormat indicates OGG is supported but for "ogg" extension.
-		// Adding "oga" too.
-		extensions.add("oga");
 	}
 
 	@Override
@@ -54,10 +54,18 @@ public class JaudiotaggerFacet implements Facet {
 
 	@Override
 	public void updateMetadata(File file, String extension, Metadatas metadatas) throws Exception {
-		// SupportedFileFormat indicates OGG is supported but for "ogg" extension.
-		// Load "oga" as "ogg".
-		String supportedExtension = "oga".equals(extension) ? "ogg" : extension;
-		AudioFile f = AudioFileIO.readAs(file, supportedExtension);
+		// Some files will generate a trace from JAudioTagger (ni Mp4BoxHeader.seekWithinLevel) using "java.util.logging"
+		// and bypassing the SLF4J configuration from Nimbus. The next line will remove this unwanted trace.
+		Logger.getLogger("org.jaudiotagger.audio.mp4.atom").setLevel(Level.OFF);
+
+		AudioFile f;
+		try {
+			f = AudioFileIO.readAs(file, extension);
+		} catch (CannotReadVideoException ex) {
+			// Some extension, like "mp4" can be either audio or video but JAudioTagger only supports audio files.
+			// If the file contains a video, JAudioTagger will throw a CannotReadVideoException, and we will simply ignore the file.
+			return;
+		}
 
 		AudioHeader h = f.getAudioHeader();
 		metadatas.put("duration", (long) (h.getTrackLength() * 1000)); // second => ms
